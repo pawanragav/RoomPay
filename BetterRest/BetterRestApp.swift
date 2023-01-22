531f26d1e691875dce77c191237856877ef27d24
Pawan Ragavendhar v <pawanharris@gmail.com>
2023-01-22 15:49:08 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pawan's Mac on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
